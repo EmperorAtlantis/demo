ad8695a8d7569a8ec3c869d54007062964470014
EmperorAtlantis <1968297406@qq.com>
2018-02-01 23:55:23 +0800

    2

--- src/main/java/com/demo2/controller/HellowController.java
+++ src/main/java/com/demo2/controller/HellowController.java
@@ -1,10 +1,12 @@
 package com.demo2.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HellowController {
+
     @RequestMapping("/hellow")
     public String sayHellow(){
         System.out.println("hellow");
